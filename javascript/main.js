40cce42b13906a76f83694fc452f49a4cfa61429
shota sato <sugergg@gmail.com>
2021-11-21 14:51:55 +0900

    Chapter8 finished.

--- javascript/main.js
+++ javascript/main.js
@@ -2,7 +2,29 @@
 if (localStorage.getItem("user-token") == null) {
   window.location.replace(document.location.origin + "/login");
 } else {
-  getItems();
+  let cachedDate = Date.parse(localStorage.getItem("item-cache-date"));
+  let now = new Date();
+  let difference = Math.round((now - cachedDate) / (1000));
+
+  if (difference <= 120) {
+    runRenderProcess(JSON.parse(localStorage.getItem("item-cache-data")));
+  } else {
+    getItems();
+  }
+
+}
+
+/**
+ * Fires a series of render processes to fill out the main view.
+ * 
+ * @param data {JSON} data from the API on the to-do items. 
+ * @returns null 
+ */
+function runRenderProcess(data) {
+  renderItems(data["pending_items"], "edit", "pendingItems", editItem);
+  renderItems(data["done_items"], "delete", "doneItems", deleteItem);
+  document.getElementById("completeNum").innerHTML = data["done_item_count"];
+  document.getElementById("pendingNum").innerHTML = data["pending_item_count"];
 }
 
 /**
@@ -48,7 +70,7 @@ function renderItems(items, processType,
  * 
  * @param {String} url -  the URL endpoint fot the API call.
  * @param {String} method - the method of the API call => POST, GET, PUT
- * @returns {XMLHTTPRequest} - the API packaged API request.
+ * @returns {XMLHttpRequest} - the API packaged API request.
  */
 function apiCall(url, method) {
   let xhr = new XMLHttpRequest();
@@ -58,14 +80,17 @@ function apiCall(url, method) {
     if (this.readyState === this.DONE) {
       if (this.status === 401) {
         window.location.replace(document.location.origin + "/login");
+      } else {
+        // Render view.
+        runRenderProcess(JSON.parse(this.responseText));
+        // Store view info as cache.
+        localStorage.setItem("item-cache-date", new Date());
+        localStorage.setItem("item-cache-data", this.responseText);
       }
-      renderItems(JSON.parse(this.responseText)["pending_items"], "edit", "pendingItems", editItem);
-      renderItems(JSON.parse(this.responseText)["done_items"],  "delete", "doneItems", deleteItem);
-      document.getElementById("completeNum").innerHTML = JSON.parse(this.responseText)["done_item_count"];
-      document.getElementById("pendingNum").innerHTML = JSON.parse(this.responseText)["pending_item_count"];
     }
   });
 
+  // Open xml http request listener. 
   xhr.open(method,"api/v1" + url);
   xhr.setRequestHeader('content-type', 'application/json');
   xhr.setRequestHeader('user-token', localStorage.getItem("user-token"));
