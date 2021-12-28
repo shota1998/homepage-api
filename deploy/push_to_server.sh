b37873ed8c830cd71c13f5a6f2636b7803b905e8
Shota Sato <sugergg@gmail.com>
2021-12-29 01:03:12 +0900

    Still in progress.

--- deploy/push_to_server.sh
+++ deploy/push_to_server.sh
@@ -1,17 +1,59 @@
 #!/usr/bin/env bash
 
+# ******************************************************
+# Move to the directory where needed files are located.
+# ******************************************************
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P)"
 cd $SCRIPTPATH
 
-scp -i "./rust_app.pem" ./docker-compose.yml ec2-user@54.178.7.242:/home/ec2-user/docker-compose.yml
-scp -i "./rust_app.pem"  -r ./nginx ec2-user@54.178.7.242:/home/ec2-user
+# ***********************
+# Copy local file to EC2.
+# ***********************
+echo -e "\n----- Start  : Copy local file. -----\n"
+scp -i "./rust_app.pem" \
+    ./docker-compose.yml ${EC2_ADDRESS_WITH_USER}:/home/ec2-user/docker-compose.yml
+scp -i "./rust_app.pem" \
+    -r ./nginx ${EC2_ADDRESS_WITH_USER}:/home/ec2-user
 
-scp -i "./rust_app.pem"  -t  ec2-user@54.178.7.242 << EOF
+# ***********************
+# Execute command at EC2.
+# ***********************
+
+echo -e  "\n----- Start  : Execute command at EC2. -----\n"
+# ssh -i "./rust_app.pem"  -t  ec2-user@54.178.7.242 << EOF
+  
+#   docker-compose up -d
+#   docker container exec \
+#     -t rust_app bash \
+#     -c "echo DATABASE_URL=postgres://username:password@todo.ccuf26pumepg.ap-northeast-1.rds.amazonaws.com/to_do > .env"
+#   docker container exec \
+#     -t rust_app diesel migration run
+
+#   rm -r nginx/
+#   rm docker-compose.yml
+# EOF
+ssh -i "./rust_app.pem" -t ${EC2_ADDRESS_WITH_USER} << EOF
+  sudo service docker start
   docker-compose up -d
-  docker container exec -t rust_app diesel migration run
-    rm -r nginx/
-    rm docker-compose.yml
+  docker container exec \
+    -t rust_app bash \
+    -c "echo DATABASE_URL=postgres://username:password@todo.ccuf26pumepg.ap-northeast-1.rds.amazonaws.com/to_do > .env"
+  docker container exec \
+    -t rust_app diesel migration run
 
+  rm -r nginx/
+  rm docker-compose.yml
 EOF
 
-#curl --header "Content-Type: application/json" --request POST --data '{"name":"test", "email":"test", "password": "test"}' 54.178.7.242/api/v1/user/create
+# *****************
+# Create test user. 
+# *****************
+echo -e "\n----- Start : Create test user. -----\n"
+curl --header  "Content-Type: application/json" \
+     --request POST \
+     --data    '{"name":"test", "email":"test", "password": "test"}' \
+               ${MY_AWS_EC2_ADDRESS}/api/v1/user/create
+
+# docker-compose stop 
+#   docker container rm rust_app
+#   docker image rm chan1998/actix_web_application
