554a810644fe8fb9039a2fc75d822f1feaf3bb99
Shota Sato <sugergg@gmail.com>
2022-01-26 21:50:14 +0900

    Create Article API

--- src/routes/article/mod.rs
+++ src/routes/article/mod.rs
@@ -1,6 +1,6 @@
 use actix_web::web;
 
-// mod create;
+mod create;
 mod get;
 // mod edit;
 // mod delete;
@@ -10,8 +10,8 @@ use super::path::Path;
 pub fn routes_factory(app: &mut web::ServiceConfig) {
   let base_path: Path = Path{prefix: String::from("/article"), backend: true};
   
-  // app.route(&base_path.define(String::from("/create")),
-  //   web::post().to(create::create));
+  app.route(&base_path.define(String::from("/create")),
+    web::post().to(create::create));
   app.route(&base_path.define(String::from("/get")),
     web::get().to(get::get));
   // app.route(&base_path.define(String::from("/edit")),
