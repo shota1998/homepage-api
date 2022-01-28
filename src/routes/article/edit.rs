53d7b2dba8e8ae0bada9338261504454f9fb212c
Shota Sato <sugergg@gmail.com>
2022-01-29 00:57:02 +0900

    Refact API

--- src/routes/article/edit.rs
+++ src/routes/article/edit.rs
@@ -1,31 +1,22 @@
 use crate::diesel;
 use diesel::prelude::*;
-
-use actix_web::{web, HttpResponse, HttpRequest};
-
-use super::utils::return_state;
-
+use actix_web::{web, HttpResponse };
 use crate::database::establish_connection;
 use crate::json_serialization::article::Article;
 use crate::schema::articles;
-// use crate::auth::jwt::JwtToken;
 
 /// This is function edits a to do item's status.
 /// 
 /// # Arguments
-/// * articles_item (web::Json<ToDoItem>): This serialize the JSON body via the ToDoItem struct
-/// * req (HttpRequest): The request being made
+/// * articles (web::Json<ToDoItem>): This serialize the JSON body via the ToDoItem struct
 /// 
 ///  # Returns
-///  (HttpResponse): response body to be passed to the viewer.
-pub async fn edit(article: web::Json<Article>, req: HttpRequest) -> HttpResponse {
-  // Extract title info from json.
+///  (HttpResponse): Response body.
+pub async fn edit(article: web::Json<Article>) -> HttpResponse {
+  // Extract info from json.
   let id_ref:    &i32    = &article.id.clone();
   let title_ref: &String = &article.title.clone();
   let body_ref:  &String = &article.body.clone();
-
-  // Extract token from HttpRequest.
-  // let token: JwtToken = JwtToken::decode_from_request(req).unwrap();
 
   // Edit item data of DB. Change the state to "done".
   let connection = establish_connection();
@@ -39,8 +30,6 @@ pub async fn edit(article: web::Json<Article>, req: HttpRequest) -> HttpResponse
                        ))
                       .execute(&connection);
                  
-  // Return items json which will be used to check deleted or not.
-  // return HttpResponse::Ok().json(return_state(&token.user_id))
   match  update_result {
     Ok(_) => HttpResponse::Created().await.unwrap(),
     Err(_) => HttpResponse::Conflict().await.unwrap()
--- src/routes/article/get.rs
+++ src/routes/article/get.rs
@@ -1,5 +1,18 @@
-use actix_web::{Responder, HttpRequest};
-use super::utils::return_state;
+use actix_web::{web, Responder};
+use crate::diesel;
+use diesel::prelude::*;
+use serde::{Deserialize};
+
+use crate::database::establish_connection;
+use crate::schema::articles;
+use crate::json_serialization::articles::Articles;
+use crate::json_serialization::article::Article;
+use crate::models::article::article::Article as Model_Article;
+
+#[derive(Deserialize)]
+pub struct RequestBody {
+    id: i32,
+}
 
 /// This view gets all of the saved to do items that are stored in the state.json file.
 ///
@@ -8,9 +21,45 @@ use super::utils::return_state;
 ///
 /// # Returns
 /// * (web::Json): all of the stored to do items
-pub async fn get(req: HttpRequest) -> impl Responder {
-  // let token: JwtToken = JwtToken::decode_from_request(req).unwrap();
+pub async fn get_all_articles() -> impl Responder {
+  let connection = establish_connection();
+  let article_models = articles::table
+        .order(articles::columns::id.asc())
+        .load::<Model_Article>(&connection)
+        .unwrap();
 
-  // return return_state(&token.user_id);
-  return return_state();
+  let mut article_buffer = Vec::new();
+
+  // Convert model to json serializable structure.
+  for article_model in article_models {
+    let article = Article::new(article_model.id,
+                               article_model.title,
+                               article_model.body);
+
+    article_buffer.push(article);
+  }
+
+  return Articles::new(article_buffer);
+}
+
+/// This view gets all of the saved to do items that are stored in the state.json file.
+///
+/// # Arguments
+/// * req (HttpRequest): 
+///
+/// # Returns
+/// * (web::Json): all of the stored to do items
+pub async fn get_article_by_id(request_body: web::Json<RequestBody>) -> impl Responder {
+  let connection = establish_connection();
+  let article_model = articles::table
+        .filter(articles::columns::id.eq(&request_body.id))
+        .order(articles::columns::id.asc())
+        .load::<Model_Article>(&connection)
+        .unwrap();
+
+  let article = Article::new(article_model[0].id.clone(),
+                             article_model[0].title.clone(),
+                             article_model[0].body.clone());
+
+  return article;
 }
