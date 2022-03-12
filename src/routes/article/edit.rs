c5dcf8f441c7ce65a82ace7c801921a8b53d1092
Shota Sato <sugergg@gmail.com>
2022-03-13 07:50:41 +0900

    New : Temporary storage of editing articles.

--- src/routes/article/edit.rs
+++ src/routes/article/edit.rs
@@ -10,7 +10,12 @@ use crate::models::article::article::Article as Model_Article;
 use crate::schema::articles;
 use crate::schema::editing_articles;
 
+// todo: The function is divided as follows.
+//         1: Obtaining an ID
+//         2: Getting the content
 // todo: test this function
+// todo: Describe common processes in the model.
+
 /// Reflect an editing_article to an article in article table.
 /// 
 /// # Arguments
@@ -67,11 +72,11 @@ pub async fn edit_article(editing_article: web::Json<EditingArticleWithoutArticl
 /// Reflect edits to the editing_article table.
 /// 
 /// # Arguments
-/// * editing_article (web::Json<EditingArticle>): This serialize the JSON body.
+/// * editing_article (web::Json<EditingArticleWithoutArticleId>): This serialize the JSON body.
 /// 
 ///  # Returns
 ///  (Responder): Content of Article.
-pub async fn edit_editing_article(editing_article: web::Json<EditingArticle>) -> impl Responder {
+pub async fn edit_editing_article(editing_article: web::Json<EditingArticleWithoutArticleId>) -> impl Responder {
   let connection = establish_connection();
 
   // Extract info from json.
@@ -99,6 +104,7 @@ pub async fn edit_editing_article(editing_article: web::Json<EditingArticle>) ->
   return article;
 }
 
+// todo: selct id from article table by article_id of editing_article.
 /// Reflesh editing article.
 /// 
 /// # Arguments
@@ -107,16 +113,22 @@ pub async fn edit_editing_article(editing_article: web::Json<EditingArticle>) ->
 ///  # Returns
 ///  (Responder): Content of EditingArticle.
 // todo 
-pub async fn reflesh_editing_article(editing_article: web::Json<EditingArticle>) -> impl Responder {
+pub async fn reflesh_editing_article(editing_article: web::Json<EditingArticleWithoutArticleId>) -> impl Responder {
   let connection = establish_connection();
 
   // Extract info from json.
-  let id_ref:         &i32    = &editing_article.id.clone();
-  let article_id_ref: &i32    = &editing_article.article_id.clone();
+  let id_ref: &i32 = &editing_article.id.clone();
+
+  // Get the column "article_id" from the table "editing_article".
+  let article_id: i32 = editing_articles::table
+                                         .filter(editing_articles::id.eq(&id_ref))
+                                         .select(editing_articles::article_id)
+                                         .first(&connection)
+                                         .unwrap();
 
   // Get an article.
   let article_model = articles::table
-                               .filter(articles::columns::id.eq(article_id_ref))
+                               .filter(articles::columns::id.eq(article_id))
                                .order(articles::columns::id.asc())
                                .get_result::<Model_Article>(&connection)
                                .unwrap();
