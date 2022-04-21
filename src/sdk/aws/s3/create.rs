bd9c6bea4fc579dd91c055939f090bf4d60b7d23
Shota Sato <sugergg@gmail.com>
2022-04-21 19:38:49 +0900

    In progress : Delete s3 objects when an article is updated. : Code UT for sdk for aws s3.

--- src/sdk/aws/s3/create.rs
+++ src/sdk/aws/s3/create.rs
@@ -1,68 +1,36 @@
-use aws_config::meta::region::RegionProviderChain;
 use aws_sdk_s3::presigning::config::PresigningConfig;
 use aws_sdk_s3::{Client, Region, PKG_VERSION};
 use std::error::Error;
 use std::time::Duration;
 
-#[derive(Debug, StructOpt)]
-struct Opt {
-    /// The AWS Region.
-    #[structopt(short, long)]
-    region: Option<String>,
-
-    /// The name of the bucket.
-    #[structopt(short, long)]
-    bucket: String,
-
-    /// The object key.
-    #[structopt(short, long)]
-    object: String,
-
-    /// How long in seconds before the presigned request should expire.
-    #[structopt(short, long)]
-    expires_in: Option<u64>,
-
-    /// Whether to display additional information.
-    #[structopt(short, long)]
-    verbose: bool,
-}
-
 /// Adds an object to a bucket and returns a public URI.
 /// # Arguments
 ///
-/// * `[-r REGION]` - The Region in which the client is created.
-///   If not supplied, uses the value of the **AWS_REGION** environment variable.
-/// * `-b BUCKET` - The bucket where the object is uploaded.
-/// * `-o OBJECT` - The name of the file to upload to the bucket.
-///   If the environment variable is not set, defaults to **us-west-2**.
-/// * `[-e EXPIRES_IN]` - The amount of time the presigned request should be valid for.
+/// * region(&str)   - The Region in which the client is created.
+/// * bucket(&str)   - The bucket where the object is uploaded.
+/// * object(&str)   - The name of the file to upload to the bucket.
+/// * duration(&u64) - The amount of time the presigned request should be valid for.
 ///   If not given, this defaults to 15 minutes.
-/// * `[-v]` - Whether to display additional information.
-pub async fn put_object(
-    region:    &str,
+pub async fn put_object<'a>(
+    region:    &'a str,
     bucket:    &str,
     object:    &str,
-    expires_in: u64
+    duration:  &u64
 ) -> Result<(), Box<dyn Error>> {
 
-    tracing_subscriber::fmt::init();
+    // tracing_subscriber::fmt::init();
 
-    let region_provider = RegionProviderChain::first_try(region.map(Region::new))
-        .or_default_provider()
-        .or_else(Region::new("us-west-2"));
-        
-    let shared_config = aws_config::from_env().region(region_provider).load().await;
-
-    let client = Client::new(&shared_config);
-
-    let expires_in = Duration::from_secs(expires_in.unwrap_or(900));
+    let region_object   = Region::new(String::from(region));
+    let shared_config   = aws_config::from_env().region(region_object).load().await;
+    let client          = Client::new(&shared_config);
+    let duration_object = Duration::from_secs(*duration);
 
     // Adds an object to a bucket and returns a public URI.
     let presigned_request = &client
         .put_object()
-        .bucket(&bucket)
-        .key(&object)
-        .presigned(PresigningConfig::expires_in(expires_in)?)
+        .bucket(bucket)
+        .key(object)
+        .presigned(PresigningConfig::expires_in(duration_object)?)
         .await?;
 
     println!();
@@ -70,7 +38,7 @@ pub async fn put_object(
     println!("Region:            {}", shared_config.region().unwrap());
     println!("Bucket:            {}", &bucket);
     println!("Object:            {}", &object);
-    println!("Expires in:        {} seconds", expires_in.unwrap_or(900));
+    println!("Expires in:        {} seconds", duration);
     println!();
     println!("Object URI: {}", presigned_request.uri());
 
