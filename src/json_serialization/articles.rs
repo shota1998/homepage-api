98ca5e6af2f4fd1e2c9ae72026605be3c9e7f439
Shota Sato <sugergg@gmail.com>
2022-04-24 13:52:49 +0900

    In progress : Compatible with actix-web 4.0.1

--- src/json_serialization/articles.rs
+++ src/json_serialization/articles.rs
@@ -1,8 +1,5 @@
 use std::vec::Vec;
 use serde::Serialize;
-use futures::future::{ready, Ready};
-use actix_web::{Responder, Error, HttpResponse, HttpRequest};
-
 use crate::json_serialization::article::Article;
 
 /// This struct packages the raw struct fields to package items for JSON serialization.
@@ -35,23 +32,5 @@ impl Articles {
     return Articles {
       articles: articles
     }
-  }
-}
-
-impl Responder for Articles {
-  type Error  = Error;
-  type Future = Ready<Result<HttpResponse, Error>>;
-  /// This function gets fired when the struct is being returned in an actix view.
-  ///
-  /// # Arguments
-  /// * _req (&HttpRequest): The request belonging to the view.
-  ///
-  /// # Returns
-  /// * (Self::Future): An OK HTTP response with the serialized struct in the body.
-  fn respond_to(self, _req: &HttpRequest) -> Self::Future {
-    let body = serde_json::to_string(&self).unwrap();
-    ready(Ok(HttpResponse::Ok()
-      .content_type("application/json")
-      .body(body)))
   }
 }
--- src/json_serialization/editing_article_without_article_id.rs
+++ src/json_serialization/editing_article_without_article_id.rs
@@ -1,7 +1,5 @@
 use serde::Deserialize;
 use serde::Serialize;
-use futures::future::{ready, Ready};
-use actix_web::{Responder, Error, HttpResponse, HttpRequest};
 
 #[derive(Deserialize, Serialize, Clone)]
 pub struct EditingArticleWithoutArticleId {
@@ -26,23 +24,5 @@ impl EditingArticleWithoutArticleId {
       title      : title,
       body       : body
     }
-  }
-}
-
-impl Responder for EditingArticleWithoutArticleId {
-  type Error  = Error;
-  type Future = Ready<Result<HttpResponse, Error>>;
-  /// This function gets fired when the struct is being returned in an actix view.
-  ///
-  /// # Arguments
-  /// * _req (&HttpRequest): The request belonging to the view.
-  ///
-  /// # Returns
-  /// * (Self::Future): An OK HTTP response with the serialized struct in the body.
-  fn respond_to(self, _req: &HttpRequest) -> Self::Future {
-    let body = serde_json::to_string(&self).unwrap();
-    ready(Ok(HttpResponse::Ok()
-      .content_type("application/json")
-      .body(body)))
   }
 }
