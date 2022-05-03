86e4a1385819a4d1c8294f681716d59cb9685edb
Shota Sato <sugergg@gmail.com>
2022-05-03 14:48:33 +0900

    In progress : Delete s3 object while reflecting an article.

--- src/routes/editing_article/get.rs
+++ src/routes/editing_article/get.rs
@@ -4,12 +4,9 @@ use diesel::prelude::*;
 use serde::{Deserialize};
 
 use crate::database::establish_connection;
-use crate::schema::{articles, editing_articles};
-use crate::json_serialization::articles::Articles;
-use crate::json_serialization::article::Article;
+use crate::schema::editing_articles;
 use crate::json_serialization::editing_articles::EditingArticles;
 use crate::json_serialization::editing_article::EditingArticle;
-use crate::models::article::article::Article                as Model_Article;
 use crate::models::article::editing_article::EditingArticle as Model_EditingArticle;
 
 #[derive(Deserialize)]
@@ -22,57 +19,6 @@ pub struct RequestParameter {
     id: i32,
 }
 
-/// Get all articles.
-///
-/// # Arguments
-/// * request_body web::Json<RequestBody>: 
-///
-/// # Returns
-/// * (Responder): A an article.
-pub async fn get_all_articles() -> impl Responder {
-  let connection = establish_connection();
-  let article_models = articles::table
-                                .order(articles::columns::id.asc())
-                                .load::<Model_Article>(&connection)
-                                .unwrap();
-
-  let mut article_buffer = Vec::new();
-
-  // Convert model to json serializable structure.
-  for article_model in article_models {
-    let article = Article::new(article_model.id,
-                               article_model.title,
-                               article_model.body);
-
-    article_buffer.push(article);
-  }
-
-  return Articles::new(article_buffer);
-}
-
-/// Get an article.
-/// Extract by id.
-///
-/// # Arguments
-/// * request_body web::Json<RequestBody>: 
-///
-/// # Returns
-/// * (Responder): A an article.
-pub async fn get_article_by_id(request_body: web::Json<RequestBody>) -> impl Responder {
-  let connection = establish_connection();
-  let article_model = articles::table
-                               .filter(articles::columns::id.eq(&request_body.id))
-                               .order(articles::columns::id.asc())
-                               .load::<Model_Article>(&connection)
-                               .unwrap();
-
-  let article = Article::new(article_model[0].id.clone(),
-                             article_model[0].title.clone(),
-                             article_model[0].body.clone());
-
-  return article;
-}
-
 /// Get all editing articles.
 ///
 /// # Arguments
@@ -80,7 +26,7 @@ pub async fn get_article_by_id(request_body: web::Json<RequestBody>) -> impl Res
 ///
 /// # Returns
 /// * (Responder): A an article.
-pub async fn get_all_editing_articles() -> impl Responder {
+pub async fn get_all() -> impl Responder {
   let connection = establish_connection();
 
   let editing_article_models = editing_articles::table
@@ -111,7 +57,7 @@ pub async fn get_all_editing_articles() -> impl Responder {
 ///
 /// # Returns
 /// * (Responder): An editing article.
-pub async fn get_editing_article_by_id(request_body: web::Json<RequestBody>) -> impl Responder {
+pub async fn get_by_id(request_body: web::Json<RequestBody>) -> impl Responder {
   let connection = establish_connection();
   
   let editing_articles_model = editing_articles::table
@@ -137,7 +83,7 @@ pub async fn get_editing_article_by_id(request_body: web::Json<RequestBody>) -> 
 /// # Returns
 /// * (Responder): An editing article.
 // pub async fn get_editing_article_by_article_id(request_body: web::Json<RequestBody>) -> impl Responder {
-pub async fn get_editing_article_by_article_id(request_parameter: web::Query<RequestParameter>) -> impl Responder {
+pub async fn get_by_article_id(request_parameter: web::Query<RequestParameter>) -> impl Responder {
   let connection = establish_connection();
   
   let editing_articles_model = editing_articles::table
