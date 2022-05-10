3a2a6a45bef8a34d39ab483bbea49c404ca0a099
Shota Sato <sugergg@gmail.com>
2022-05-10 22:55:03 +0900

    In progress: UT with rollback.

--- src/routes/editing_article/reflect.rs
+++ src/routes/editing_article/reflect.rs
@@ -5,6 +5,7 @@ use diesel::connection::TransactionManager;
 use actix_web::{web, HttpResponse};
 use std::env;
 use dotenv::dotenv;
+use aws_sdk_s3::Error as S3Error;
 use crate::database::establish_connection;
 use crate::json_serialization::editing_article::EditingArticle;
 use crate::json_serialization::editing_article_without_article_id::EditingArticleWithoutArticleId;
@@ -30,13 +31,16 @@ pub async fn reflect(editing_article: web::Json<EditingArticleWithoutArticleId>)
   let title_ref: &String = &editing_article.title.clone();
   let body_ref:  &String = &editing_article.body.clone();
 
+  let mut editing_article: EditingArticle;
+
   // Begin transaction.
   let c  = establish_connection();
   let tm = c.transaction_manager();
 
+  // ---------------------
+  // Reflect article.
+  // ---------------------
   match async {
-    type E = Error;
-
     tm.begin_transaction(&c)?;
 
     // Reflect edits to the editing_article table.
@@ -48,8 +52,7 @@ pub async fn reflect(editing_article: web::Json<EditingArticleWithoutArticleId>)
                                   editing_articles::columns::title.eq(&title_ref),
                                   editing_articles::columns::body .eq(&body_ref)
                                 ))
-                                .get_result::<Model_EditingArticle>(&c)
-                                .unwrap();
+                                .get_result::<Model_EditingArticle>(&c)?;
 
     // Reflect edits to the article table.
     let filtered_article = articles::table
@@ -60,18 +63,34 @@ pub async fn reflect(editing_article: web::Json<EditingArticleWithoutArticleId>)
                         articles::columns::title.eq(&title_ref),
                         articles::columns::body.eq(&body_ref)
                         ))
-                        .get_result::<Model_Article>(&c)
-                        .unwrap();
+                        .get_result::<Model_Article>(&c)?;
 
     // todo: move this to trait.
-    let editing_article = EditingArticle::new(
-                            editing_article_model.id.clone(),
-                            editing_article_model.article_id.clone(),
-                            editing_article_model.title.clone(),
-                            editing_article_model.body.clone()
-                          );
+    editing_article = EditingArticle::new(
+                        editing_article_model.id.clone(),
+                        editing_article_model.article_id.clone(),
+                        editing_article_model.title.clone(),
+                        editing_article_model.body.clone()
+                      );
 
-    // Delete S3 objects.
+    Ok(())
+  }
+  .await
+  {
+    Ok(_)  => (),
+    Err(_) => match tm.rollback_transaction(&c) {
+        Ok(_)  => return HttpResponse::InternalServerError().await.unwrap(),
+        Err(_) => return HttpResponse::InternalServerError().await.unwrap(),
+      },
+  };
+
+  // ---------------------
+  // Delete S3 objects.
+  // ---------------------
+  // 2022/05/10 22:50
+  // I don't know how to handle different types of errors at the same time.
+  // Instead, handle different types of errors separately.
+  match async{
     dotenv().ok();
 
     let aws_client  = &get_aws_client().unwrap();
@@ -79,13 +98,13 @@ pub async fn reflect(editing_article: web::Json<EditingArticleWithoutArticleId>)
     let object_keys_to_be_deleted: Vec<String> = 
           extract_object_keys_to_be_deleted(&article_model.body, &editing_article_model.body);
 
-    delete_objects(
-        aws_client, 
-        bucket_name, 
-        object_keys_to_be_deleted
-      ).await?;
+    let delete_objects_succeded = delete_objects(
+                                    aws_client, 
+                                    bucket_name, 
+                                    object_keys_to_be_deleted
+                                  ).await.unwrap();
 
-    Ok::<EditingArticle, E>(editing_article)
+    Ok::<EditingArticle, Error>(editing_article)
   }
   .await
   {
