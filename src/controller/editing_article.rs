f9fdd0f55e60f1a00127b786934555295b8a972e
Shota Sato <sugergg@gmail.com>
2022-05-23 08:27:44 +0900

    In progress : Split route into logic and controller.

--- src/controller/editing_article.rs
+++ src/controller/editing_article.rs
@@ -1,13 +1,20 @@
 pub async fn reflect()  {
-
+  // Begin transaction.
+  // let article_model         = article_model::update();
+  // let editing_article_model = editing_article_model::update();
+  // let editing_article       = editing_article::new(editing_article_model);
+  // error handling.
+  // delete object.
+  // errro handling.
 }
 
 #[cfg(test)]
-mod routes_edting_article_reflect {
+mod controller_edting_article_reflect {
   use super::*;
 
   #[actix_web::test]
   async fn test_reflect() {
+    
     reflect();
     assert_eq!(true, true);
   }
