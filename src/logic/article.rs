9b7d934ad770afc60abc4ea42047a78ff5e6098e
Shota Sato <sugergg@gmail.com>
2022-05-24 08:57:52 +0900

    In progress : Split route into logic and controller.

--- src/logic/article.rs
+++ src/logic/article.rs
@@ -3,9 +3,25 @@ use diesel::prelude::*;
 use diesel::pg::PgConnection;
 use crate::schema::articles;
 use crate::models::article::{
-  editing_article::EditingArticle,
-  article::Article
+  new_article::NewArticle,
+  article::Article,
+  editing_article::EditingArticle
 };
+
+/// Creates an article.
+///
+/// # Arguments
+/// * req(HttpRequest): the HTTP request passed into the view
+/// 
+/// # Returns
+/// * (impl Responder): message to be sent back to the user. 
+pub async fn create(new_article: NewArticle, c: &PgConnection) -> Article {
+
+  diesel::insert_into(articles::table)
+         .values(&new_article)
+         .get_result::<Article>(c)
+         .unwrap()
+}
 
 /// Update an article.
 /// 
@@ -17,7 +33,9 @@ use crate::models::article::{
 pub async fn update(editing_article: EditingArticle, c: &PgConnection) -> Article {
   
   let filtered_article = articles::table
-                         .filter(articles::columns::id.eq(editing_article.article_id));
+                         .filter(articles::columns::id.eq(
+                           editing_article.article_id
+                          ));
 
   diesel::update(filtered_article)
          .set((
@@ -33,21 +51,43 @@ mod logic_article {
   use super::*;
   use diesel::result::Error;
   use crate::database::establish_connection;
+  use crate::models::article::{
+    new_article::NewArticle,
+    article::Article,
+    editing_article::EditingArticle
+  };
+
+  fn create_new_article_model() -> NewArticle {
+    NewArticle::new("test title".to_owned(), "test body".to_owned())
+  }
 
   #[actix_web::test]
-  async fn test_update() {
+  async fn test_create() {
     let c = establish_connection();
 
     c.test_transaction::<_, Error, _>(|| {
-      // todo: create article
-      // todo: update editing_article, but use mock.
-      // todo: get editing_article
-      let editing_article = editing_article::get(editing_article_id);
+      let model   = create_new_article_model();
+      let article = create(model, &c).await;
       
-      assert_eq!(true, true);
-      update(editing_article, &c);
-
+      assert_eq!(model, article);
       Ok(())
     })
   }
+
+  // #[actix_web::test]
+  // async fn test_update() {
+  //   let c = establish_connection();
+
+  //   c.test_transaction::<_, Error, _>(|| {
+  //     // todo: create article
+  //     // todo: update editing_article, but use mock.
+  //     // todo: get editing_article
+  //     let editing_article = editing_article::get(editing_article_id);
+      
+  //     update(editing_article, &c);
+
+  //     assert_eq!(true, true);
+  //     Ok(())
+  //   })
+  // }
 }
