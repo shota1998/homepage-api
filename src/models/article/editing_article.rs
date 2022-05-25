25e502c390bd06f43bc66e836d89a0f06adf80f6
Shota Sato <sugergg@gmail.com>
2022-05-25 23:52:35 +0900

    In progress : Split route into logic and controller.

--- src/models/article/editing_article.rs
+++ src/models/article/editing_article.rs
@@ -1,6 +1,9 @@
 use crate::schema::editing_articles;
+use crate::json_serialization::editing_article::EditingArticle as Serial_EditingArticle;
+use actix_web::web;
 
 #[derive(Queryable, Identifiable, Associations)]
+#[derive(Clone)]
 #[table_name="editing_articles"]
 pub struct EditingArticle {
   pub id         : i32,
@@ -21,6 +24,21 @@ impl EditingArticle {
   /// # Returns
   /// (NewUser) :
   pub fn new(id: i32, article_id: i32, title: String, body: String) -> EditingArticle {
-    return EditingArticle {id, article_id, title, body};
+    return EditingArticle {
+      id:         id,
+      article_id: article_id,
+      title:      title,
+      body:       body
+    };
+  }
+
+  // todo: move to trait with new()
+  pub fn new_by_json(editing_article_model: web::Json<Serial_EditingArticle>) -> EditingArticle {
+    return EditingArticle {
+      id:         editing_article_model.id.clone(),
+      article_id: editing_article_model.article_id.clone(),
+      title:      editing_article_model.title.clone(),
+      body:       editing_article_model.body.clone()
+    };
   }
 }
