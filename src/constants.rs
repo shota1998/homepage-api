ed991f38639a95d508cf0ef0e743c6e56c5a7a3b
Shota Sato <sugergg@gmail.com>
2022-05-31 07:13:36 +0900

    In progress : Split route into logic and controller.

--- src/constants.rs
+++ src/constants.rs
@@ -1,6 +1,9 @@
+#![allow(unused)]
+
 // SUCCEEDED
 pub const REFLECT_SUCCEEDED: &str = "Reflect succeeded.";
 
 // FAILED
-pub const REFLECT_FAILED: &str   = "Reflect failed.";
-pub const MIGRATION_FAILED: &str = "Migration failed.";
+pub const REFLECT_FAILED:  &str = "Reflect failed.";
+pub const COMMIT_FAILED:   &str = "Commit failed.";
+pub const ROLLBACK_FAILED: &str = "Rollback failed.";
