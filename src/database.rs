bef196e57b5597b79267158401fbdf55366793cc
shota sato <sugergg@gmail.com>
2022-09-04 18:15:54 +0900

    Remove DATABASE_URL_DEV

--- src/database.rs
+++ src/database.rs
@@ -3,23 +3,16 @@ use diesel::prelude::*;
 use dotenv::dotenv;
 use std::env;
 
-/// Initialize database connection.
-/// # Returns
-/// * (PgConnection)
+
 pub fn establish_connection() -> PgConnection {
   dotenv().ok();
-
-  // let database_url = env::var("DATABASE_URL").expect("DATABASE_URL must be set.");
   
   let database_url = 
       if cfg!(test) {
         env::var("DATABASE_URL_TEST").expect("DATABASE_URL_TEST must be set.")
       } 
-      else if cfg!(debug_assertions) {
-        env::var("DATABASE_URL_DEV").expect("DATABASE_URL_DEV must be set.")
-      } 
       else {
-        env::var("DATABASE_URL_PROD").expect("DATABASE_URL_PROD must be set.")
+        env::var("DATABASE_URL").expect("DATABASE_URL must be set.")
       };
 
   log::info!("DB URL : {}", database_url);
